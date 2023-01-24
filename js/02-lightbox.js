6b7c152d479878004468bcaa00faa36a26e2cdfc
Rudakova1007 <110676149+Rudakova1007@users.noreply.github.com>
2023-01-24 16:18:48 +0200

    homework is done

---
+++ js/02-lightbox.js
@@ -0,0 +1,22 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+const imagesMarkup = createGalleryMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createGalleryMarkup(galleryItems){
+    return galleryItems.map(({preview, original, description})=>{
+        return `<li><a class='gallery__link' href='${original}'>
+        <img  class='gallery__image' src ='${preview}' alt ='${description}'>
+        </a></li>`;}).join('');
+}
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
